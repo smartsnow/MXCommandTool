eb609eb743b7e2e7e5b567afa7f7105c7caa7c5b
yangsw <yangsw@mxchip.com>
2019-08-16 09:46:53 +0800

    增加 MxHexStrEdit 类，实现 HEX/String 转换的 Edit 控件

--- Command/Socket/write.py
+++ Command/Socket/write.py
@@ -6,11 +6,11 @@ class Command():
     def getWidget(self):
         self.widget = MxArgsWidget('Send data on a socket.')
         self.widget.addArg('FD', QLineEdit(), 'File Descriptor of a socket.')
-        self.widget.addArg('Data', QPlainTextEdit(),'Data to be sent on a socket.')
+        self.widget.addArg('Data', MxHexStrEdit(), 'Data to be sent on a socket.')
         return self.widget
 
     def getArgs(self):
-        return {'FD': self.widget.getArgWidget('FD').text(), 'Data': self.widget.getArgWidget('Data').toPlainText()}
+        return {'FD': self.widget.getArgWidget('FD').text(), 'Data': self.widget.getArgWidget('Data').text()}
 
     def parse(self, data):
         pass
--- mxArgWidgets.py
+++ mxArgWidgets.py
@@ -26,3 +26,35 @@ class MxArgsWidget(QWidget):
 
     def getArgWidget(self, name):
         return self.argWidgetDict[name]
+
+
+class MxHexStrEdit(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        vlayout = QVBoxLayout(self)
+        vlayout.setContentsMargins(0, 0, 0, 0)
+        self.buttonToggle = QPushButton('Convert to HEX')
+        vlayout.addWidget(self.buttonToggle)
+        self.plainTextEdit = QPlainTextEdit()
+        vlayout.addWidget(self.plainTextEdit)
+        self.buttonToggle.clicked.connect(self.toggleFormat)
+
+    def toggleFormat(self):
+        if self.buttonToggle.text() == 'Convert to HEX':
+            self.buttonToggle.setText('Convert to STRING')
+            strText = self.plainTextEdit.toPlainText()
+            hexText = strText.encode().hex().upper()
+            self.plainTextEdit.setPlainText(hexText)
+        else:
+            hexText = self.plainTextEdit.toPlainText()
+            try:
+                strText = bytes.fromhex(hexText).decode()
+            except Exception as e:
+                QMessageBox.warning(self, '', 'Invalid HEX Format!\n\n%s'%(e), QMessageBox.Yes, QMessageBox.Yes)
+                return
+            self.plainTextEdit.setPlainText(strText)
+            self.buttonToggle.setText('Convert to HEX')
+
+    def text(self):
+        return self.plainTextEdit.toPlainText()
